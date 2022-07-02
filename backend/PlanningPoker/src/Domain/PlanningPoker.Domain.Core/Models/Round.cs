d768dd5bce280465c1ec2361ea7290dc7c7c2250
CleitonSM07 <cleitonsilvamiguel@hotmail.com>
2022-07-02 01:34:24 -0300

    adjust round entity

--- backend/PlanningPoker/src/Domain/PlanningPoker.Domain.Core/Models/Round.cs
+++ backend/PlanningPoker/src/Domain/PlanningPoker.Domain.Core/Models/Round.cs
@@ -2,28 +2,28 @@
 {
     public class Round
     {
-        public Round(Guid id, Guid gameId, string roundName, bool active)
+        public Round(Guid id, Guid gameId, string name, bool active)
         {
-            SetInitialValues(id, gameId, roundName, active);
+            SetInitialValues(id, gameId, name, active);
         }
 
-        public Round(Guid gameId, string roundName)
+        public Round(Guid gameId, string name)
         {
-            SetInitialValues(Guid.NewGuid(), gameId, roundName, true);
+            SetInitialValues(Guid.NewGuid(), gameId, name, true);
         }
 
-        public void SetInitialValues(Guid id, Guid gameId, string roundName, bool active)
+        public void SetInitialValues(Guid id, Guid gameId, string name, bool active)
         {
             Id = id;
             GameId = gameId;
-            RoundName = roundName?.Trim();
+            Name = name?.Trim();
             Active = active;
         }
 
         public Guid Id { get; private set; }
         public Guid GameId { get; private set; }
         public Game Game { get; private set; }
-        public string RoundName { private get; set; }
+        public string Name { private get; set; }
         public bool Active { get; private set; }
     }
 }
